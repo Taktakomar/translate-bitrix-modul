cd1bbebc59398a5cfbb8ded16c2b9acaabbca769
Taktakomar <omar2tak@gmail.com>
2022-01-28 18:04:16 +0100

    Layout erstellen & styling

--- app.js
+++ app.js
@@ -2,6 +2,32 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    const textarea = document.querySelector('.translateTextarea');
+    const leftSideContainer = document.querySelector('.leftSideContainer');
+
+    textarea.addEventListener('focus', (event) => {
+        leftSideContainer.classList.add("activeContainer");
+    });
+
+    textarea.addEventListener('focusout', (event) => {
+        leftSideContainer.classList.remove("activeContainer");
+    });
+
 
 }, false);
+
+
+
+function onKeyUP(element) {
+
+    const textPlaceholder = document.querySelector('.textPlaceholder');
+
+    if (element.innerText == "") {
+        textPlaceholder.style.display = "block";
+    } else {
+        textPlaceholder.style.display = "none";
+    }
+
+
+}
 
