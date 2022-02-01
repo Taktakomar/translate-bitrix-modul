ff07f0c9636eeb4a1f2743de7116e79d112fc218
Taktakomar <omar2tak@gmail.com>
2022-02-01 22:54:24 +0100

    Sprachliste ein- und ausblenden

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    $( "#launchModal" ).click();
+    $("#launchModal").click();
+    $("#leftSideLanguageList").hide();
+    $("#rightSideLanguageList").hide();
 
     const textarea = document.querySelector('.translateTextarea');
     const leftSideContainer = document.querySelector('.leftSideContainer');
@@ -13,4 +15,28 @@ $( document ).ready(function() {
         leftSideContainer.classList.remove("activeContainer");
     });
 
+
+
 });
+
+
+function onLanguageButtonClick(element) {
+    if (element.id == "leftLanguageButton") {
+        $("#leftSideLanguageList").show(500);
+    } else if (element.id == "rightLanguageButton") {
+        $("#rightSideLanguageList").show(500);
+    }
+}
+$(document).mouseup(function (e) {
+    var containerLeft = $("#leftLanguageButton");
+    if (!containerLeft.is(e.target) && containerLeft.has(e.target).length === 0) {
+        $("#leftSideLanguageList").hide(400);
+    }
+
+    var containerRight = $("#rightLanguageButton");
+    if (!containerRight.is(e.target) && containerRight.has(e.target).length === 0) {
+        $("#rightSideLanguageList").hide(400);
+    }
+});
+
+
